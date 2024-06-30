378941caafa6322d38fdba4bfdb3b187e4b937db
Jelle Streekstra <jelle.streekstra@hotmail.com>
2024-06-30 15:55:42 +0200

    Sepeate sub_8051EE0 into its own file

--- src/option_screens.c
+++ src/option_screens.c
@@ -4,3 +4,7 @@
 #ifndef NONMATCHING
 asm_unified(".include \"asm/nonmatching/generate_window_bg_8051B98.s\"");
 #endif
+
+#ifndef NONMATCHING
+asm_unified(".include \"asm/nonmatching/sub_8051EE0.s\"");
+#endif
